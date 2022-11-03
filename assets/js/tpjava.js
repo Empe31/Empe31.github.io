9e05e6abcfa06b79f9052c4d2bd3aa7de25ddd89
Ale Salvia <alesalvia31@gmail.com>
2022-11-03 17:32:39 -0300

    js

---
+++ assets/js/tpjava.js
@@ -0,0 +1,37 @@
+let inputNombre = document.getElementById('nombre');
+let inputApellido = document.getElementById('apellido');
+let inputCorreo = document.getElementById('correo');
+let inputCantidad = document.getElementById('cantidad');
+let inputCategoria = document.getElementById('categoria');
+let inputResumen = document.getElementById('resumen');
+let inputBorrar = document.getElementById('borrar');
+let aPagar = document.getElementById('pagar');
+
+function descuento(categoria){
+    switch(categoria){
+        case "1": return 0.2;
+        case "2": return 0.5;
+        case "3": return 0.85;
+    }
+}
+function aplicaDescuento(cantidad, categoria){
+    let precio = cantidad*200;
+    return precio*Number(descuento(categoria));
+
+}
+function validador(){
+    
+}
+
+inputResumen.addEventListener('click', (e)=>{e.preventDefault();
+aPagar.innerHTML = `${aplicaDescuento(inputCantidad.value, inputCategoria.value)}`;
+})
+
+inputBorrar.addEventListener('click', (e)=>{e.preventDefault();
+    inputNombre.value="";
+    inputApellido.value="";
+    inputCorreo.value="";
+    inputCantidad.value="";
+    aPagar.innerHTML = "";
+    categoria.selectedIndex = 0
+})
